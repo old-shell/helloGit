67b37fddd4d5e10a3e03a7d061877d8e2f8bfc12
HeJingLing <1531736116@qq.com>
2020-05-25 10:21:13 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author hjl
+ * @create 2020-05-25-10:10
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
